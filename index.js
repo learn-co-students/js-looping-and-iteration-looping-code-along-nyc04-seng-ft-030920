3131e2de95ab2b74aafd13ac235f3f038aa18f9d
Xavier Carty <xaviercarty@gmail.com>
2020-04-19 19:23:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names , event) {
+  var array = [] 
+  for(var i = 0; i < names.length; i++){
+    array.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+
+  return array
+}
+
+function countDown(number) {
+   let  i = 0;
+  while(i <= number) {
+      
+    console.log(number)
+    number--
+
+
+  }
+     
+
+}
