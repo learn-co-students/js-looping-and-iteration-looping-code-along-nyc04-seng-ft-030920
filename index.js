c829bd2044fe5321f9a21ef782b7e951927edacd
Section9-Togusa <corbindac@gmail.com>
2020-04-26 03:41:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(attending_guest, event) {
+let cards = []
+for (let i = 0; i < attending_guest.length; i++) {
+    cards.push(`Thank you, ${attending_guest[i]}, for the wonderful ${event} gift!`)
+}
+    return cards
+}
+
+function countDown(timer) {
+    while (timer > 0) { 
+        console.log(timer)
+        timer -= 1
+    }
+    console.log(timer)
+}
